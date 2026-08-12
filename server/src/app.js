8b28db473c71a4e191ba48fd2fb05b161ff90e37
unknown <ablazerabbi159@gmail.com>
2026-08-13 03:26:38 +0600

    registration back-end

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const propertyRoutes = require("./routes/propertyRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/properties", propertyRoutes);
+app.use("/api/auth", authRoutes);
 
 app.get("/api/health", (req, res) => {
   res.json({
