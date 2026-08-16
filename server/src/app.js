c21f6cb18d8f0d9eb37a57cea0dde8a3e2f5330d
rabby159 <ablazerabbi159@gmail.com>
2026-08-17 02:34:08 +0600

    allow production

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://real-estate-hub-eta.vercel.app"],
     credentials: true,
   }),
 );
